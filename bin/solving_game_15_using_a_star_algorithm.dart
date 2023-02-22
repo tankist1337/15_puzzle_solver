bce210d78aca1aaae5c0138165b699f1dfb3b1fd
Aleksey Zdanovich <alekseyzdanovich0@gmai.com>
2023-02-22 21:20:04 +0300

    ListTile was replaced by MultiArrayTile

--- bin/solving_game_15_using_a_star_algorithm.dart
+++ bin/solving_game_15_using_a_star_algorithm.dart
@@ -3,22 +3,26 @@ import 'package:solving_game_15_using_a_star_algorithm/tile/solver/tile_solver.d
 import 'package:solving_game_15_using_a_star_algorithm/tile/tile.dart';
 
 void main(List<String> arguments) {
-  List<List<int>> startMatrix = [
-    [5, 1, 9, 3],
-    [11, 13, 6, 8],
-    [14, 10, 4, 15],
-    [0, 12, 7, 2]
-  ];
+  // List<List<int>> startMatrix = [
+  //   [4, 1, 5],
+  //   [3, 7, 2],
+  //   [6, 8, 0]
+  // ];
 
-  List<List<int>> resultMatrix = [
-    [1, 2, 3, 4],
-    [5, 6, 7, 8],
-    [9, 10, 11, 12],
-    [13, 14, 15, 0]
-  ];
+  // List<List<int>> resultMatrix = [
+  //   [1, 2, 3],
+  //   [4, 5, 6],
+  //   [7, 8, 0]
+  // ];
 
-  Tile startTile = MultiArrayTile(startMatrix);
-  Tile resultTile = MultiArrayTile(resultMatrix);
+  // Tile startTile = MultiArrayTile(startMatrix);
+  // Tile resultTile = MultiArrayTile(resultMatrix);
+
+  List<int> startList = [4, 6, 2, 7, 3, 5, 8, 1, 0];
+  List<int> resultList = [1, 2, 3, 4, 5, 6, 7, 8, 0];
+
+  Tile startTile = ListTile(startList);
+  Tile resultTile = ListTile(resultList);
 
   TileSolver tileSolver = TileSolverImpl();
 
@@ -38,7 +42,7 @@ void main(List<String> arguments) {
   int seconds = stopwatch.elapsed.inSeconds - hour * 60 * 60 - minutes * 60;
 
   print(
-      "${hour != 0 ? "$hour h, " : ""}${minutes != 0 ? "$minutes m, " : ""}$seconds s");
+      "time: ${hour != 0 ? "$hour h, " : ""}${minutes != 0 ? "$minutes m, " : ""}$seconds s");
 
   solutionPathDisplay.display(solutionPath);
 }
